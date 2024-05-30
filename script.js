59380f6760d8f0621ff2d6c351f44742d36f53c9
HudaAden <147623820+HudaAden@users.noreply.github.com>
2024-05-30 08:42:53 -0500

    V3

---
+++ script.js
@@ -0,0 +1,62 @@
+const button = document.createElement('button');
+button.textContent = 'Click Me!';
+document.body.appendChild(button);
+button.addEventListener('click', () => {
+    alert('New button clicked!');
+});
+
+const secButton = document.createElement('button');
+secButton.textContent = 'Click Me!';
+document.body.appendChild(secButton);
+secButton.addEventListener('click', () => {
+    alert('New button clicked!');
+});
+
+const thiButton = document.createElement('button');
+thiButton.textContent = 'Click Me!';
+document.body.appendChild(thiButton);
+thiButton.addEventListener('click', () => {
+    alert('New button clicked!');
+});
+
+const forButton = document.createElement('button');
+forButton.textContent = 'Click Me!';
+document.body.appendChild(forButton);
+forButton.addEventListener('click', () => {
+    alert('New button clicked!');
+});
+
+const fifButton = document.createElement('button');
+fifButton.textContent = 'Click Me!';
+document.body.appendChild(fifButton);
+fifButton.addEventListener('click', () => {
+    alert('New button clicked!');
+});
+
+const siButton = document.createElement('button');
+siButton.textContent = 'Click Me!';
+document.body.appendChild(siButton);
+siButton.addEventListener('click', () => {
+    alert('New button clicked!');
+});
+
+const sevButton = document.createElement('button');
+sevButton.textContent = 'Click Me!';
+document.body.appendChild(sevButton);
+sevButton.addEventListener('click', () => {
+    alert('New button clicked!');
+});
+
+const eigButton = document.createElement('button');
+eigButton.textContent = 'Click Me!';
+document.body.appendChild(eigButton);
+eigButton.addEventListener('click', () => {
+    alert('New button clicked!');
+});
+
+const ninButton = document.createElement('button');
+ninButton.textContent = 'Click Me!';
+document.body.appendChild(ninButton);
+ninButton.addEventListener('click', () => {
+    alert('New button clicked!');
+});
